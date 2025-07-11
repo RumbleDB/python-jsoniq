9c680b3f95532b292437ee04ec5727a9ee3e933d
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2025-07-11 11:50:12 +0200

    This should now work

--- src/jsoniq/session.py
+++ src/jsoniq/session.py
@@ -1,9 +1,13 @@
 from pyspark.sql import SparkSession
 from .sequence import SequenceOfItems
+import importlib.resources as pkg_resources
+
+with pkg_resources.path("jsoniq.jars", "rumbledb-1.24.0.jar") as jar_path:
+    print("Found Rumble JAR at:", jar_path)
+    jar_path_str = "file://" + str(jar_path)
 
 class MetaRumbleSession(type):
     def __getattr__(cls, item):
-        print(f"Dynamically handled: {item}")
         if item == "builder":
             return cls._builder
         else:
@@ -11,47 +15,39 @@ class MetaRumbleSession(type):
     
 class RumbleSession(object, metaclass=MetaRumbleSession):
     def __init__(self, spark_session: SparkSession):
-        print("Initializing RumbleSession object");
         self._sparksession = spark_session
         self._jrumblesession = spark_session._jvm.org.rumbledb.api.Rumble(spark_session._jsparkSession)
 
     class Builder:
         def __init__(self):
-            self._sparkbuilder = SparkSession.builder.config("spark.jars", "file:///Users/ghislain/Code/rumble/target/rumbledb-1.24.0-jar-with-dependencies.jar")
+            self._sparkbuilder = SparkSession.builder.config("spark.jars", jar_path_str)
 
         def getOrCreate(self):
-            print("getOrCreate called");
             return RumbleSession(self._sparkbuilder.getOrCreate())
         
         def appName(self, name):
-            print(f"Setting app name: {name}");
             self._sparkbuilder = self._sparkbuilder.appName(name);
             return self;
 
         def master(self, url):
-            print(f"Setting master URL: {url}");
             self._sparkbuilder = self._sparkbuilder.master(url);
             return self;
     
         def config(self, key, value):
-            print(f"Setting config: {key} = {value}");
             self._sparkbuilder = self._sparkbuilder.config(key, value);   
             return self;
 
         def config(self, conf):
-            print(f"Setting config: {conf}");
             self._sparkbuilder = self._sparkbuilder.config(conf);   
             return self;
 
         def __getattr__(self, name):
-            print(f"Calling attribute: {name}");
             res = getattr(self._sparkbuilder, name);
             return res;
 
     _builder = Builder()
 
     def bindDataFrameAsVariable(self, name: str, df):
-        print(f"Binding DataFrame as variable: {name}");
         conf = self._jrumblesession.getConfiguration();
         if not name.startswith("$"):
             raise ValueError("Variable name must start with a dollar symbol ('$').")
@@ -61,8 +57,7 @@ class RumbleSession(object, metaclass=MetaRumbleSession):
 
     def jsoniq(self, str):
         sequence = self._jrumblesession.runQuery(str);
-        return SequenceOfItems(sequence);
+        return SequenceOfItems(sequence, self._sparksession.sparkContext);
 
     def __getattr__(self, item):
-        print(f"Accessing attribute: {item}")
         return getattr(self._sparksession, item)
