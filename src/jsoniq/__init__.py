cdd8066ddd99d27cb40500ccdb59d8e79cecdbe2
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2025-07-29 18:04:02 +0200

    Remove import

--- src/jsoniq/__init__.py
+++ src/jsoniq/__init__.py
@@ -1,8 +1,3 @@
 from jsoniq.session import RumbleSession
-from jsoniq.magic import JSONiqMagic
 
-__all__ = ["RumbleSession", "JSONiqMagic"]
-
-def load_ipython_extension(ipython):
-    rumble = RumbleSession.builder.getOrCreate();
-    ipython.register_magics(JSONiqMagic)
+__all__ = ["RumbleSession"]
