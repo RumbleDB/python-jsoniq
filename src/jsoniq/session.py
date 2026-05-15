9e7cac15bb151634488b83be2604e9a3b527adb0
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2026-05-15 17:17:33 +0200

    Fix DataFrame conversion issue.

--- src/jsoniq/session.py
+++ src/jsoniq/session.py
@@ -7,7 +7,7 @@ import re
 import pandas as pd
 from importlib.resources import files, as_file
 
-with as_file(files("jsoniq.jars").joinpath("rumbledb-2.1.0.jar")) as jar_path:
+with as_file(files("jsoniq.jars").joinpath("rumbledb-2.1.1.jar")) as jar_path:
     if (os.name == 'nt'):
         jar_path_str = str(jar_path)
     else:
