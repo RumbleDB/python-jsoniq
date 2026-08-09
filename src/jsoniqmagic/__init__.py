4dd736e868ab6aa258c5c461cf9cf2f115daac5d
McLavish <Luca27ita@gmail.com>
2026-08-09 15:56:30 +0200

    remove implicit rumbledb init. this requires the user to explicity set up a rumbledb instance.

--- src/jsoniqmagic/__init__.py
+++ src/jsoniqmagic/__init__.py
@@ -4,6 +4,4 @@ from jsoniqmagic.magic import JSONiqMagic
 __all__ = ["JSONiqMagic"]
 
 def load_ipython_extension(ipython):
-    rumble = RumbleSession.builder.getOrCreate();
-    rumble.getRumbleConf().setResultSizeCap(10);
     ipython.register_magics(JSONiqMagic)
